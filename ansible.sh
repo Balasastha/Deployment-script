0b1a08cdca73970c7d4c37fd248f8f204460328e
Balasastha P <69665880+Balasastha@users.noreply.github.com>
2025-03-15 15:06:56 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,3 +2,6 @@
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+sudo mkdir -p /etc/ansible
+sudo touch /etc/ansible/hosts
+sudo chmod 644 /etc/ansible/hosts
